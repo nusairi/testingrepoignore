fe6c2cdcff68b448f7a5bbe0ea8d56cd4a4a6ecc
nusairi <nusairi@outlook.com>
2017-04-24 21:43:58 +0100

    Add project files.

---
+++ playSampleapp/Create.feature
@@ -0,0 +1,33 @@
+﻿Feature: Create
+	In order to add a new computer to the Play sample application 
+	I would like to have a link on a home page so that I can navigate to the "Add a new computer page" and I should be able to add a new computer to the application. 
+
+@mytag
+Scenario: Navigate to "Add a computer page"
+
+Given that I am on a home page of computer data base application 
+And Add a new computer button is displayed 
+When I click on the button 
+Then I should be on the Add computer page 
+	
+
+Scenario Outline: Add computer page fields 
+Given that I am on add computer page
+Then All the <Fields> should be displayed
+Examples: 
+| Fields            |
+| Computer name     |
+| Introduced date   |
+| Discontinued date |
+
+Scenario: Add computer page company dropdown
+Given that I am on add computer page
+When I click on the dropdown menu
+Then it should display list of companies
+When I choose 'RCA'
+Then it should select 'RCA'
+
+
+
+
+
---
+++ playSampleapp/CreateSteps.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using TechTalk.SpecFlow;
+
+namespace playSampleapp
+{
+    [Binding]
+    public class CreateSteps
+    {
+        [Given(@"that I am on a home page of computer data base application")]
+        public void GivenThatIAmOnAHomePageOfComputerDataBaseApplication()
+        {
+            ScenarioContext.Current.Pending();
+        }
+        
+        [Given(@"Add a new computer button is displayed")]
+        public void GivenAddANewComputerButtonIsDisplayed()
+        {
+            ScenarioContext.Current.Pending();
+        }
+        
+        [When(@"I click on the button")]
+        public void WhenIClickOnTheButton()
+        {
+            ScenarioContext.Current.Pending();
+        }
+        
+        [Then(@"I should be on the Add computer page")]
+        public void ThenIShouldBeOnTheAddComputerPage()
+        {
+            ScenarioContext.Current.Pending();
+        }
+
+        [Given(@"that I am on add computer page")]
+        public void GivenThatIAmOnAddComputerPage()
+        {
+            ScenarioContext.Current.Pending();
+        }
+
+        [Given(@"All the (.*) are displayed")]
+        public void GivenAllTheAreDisplayed(string p0)
+        {
+            ScenarioContext.Current.Pending();
+        }
+
+        [When(@"I add data on the fields")]
+        public void WhenIAddDataOnTheFields()
+        {
+            ScenarioContext.Current.Pending();
+        }
+
+        [When(@"click on create this computer button")]
+        public void WhenClickOnCreateThisComputerButton()
+        {
+            ScenarioContext.Current.Pending();
+        }
+
+        [Then(@"it should save the details")]
+        public void ThenItShouldSaveTheDetails()
+        {
+            ScenarioContext.Current.Pending();
+        }
+
+    }
+
+}
+
+
